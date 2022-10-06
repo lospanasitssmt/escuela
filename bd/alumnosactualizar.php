b90fc3c587aec4739f99050dcebb7912535c3530
liz <lizethmendezperez@gmail.com>
2022-10-05 23:36:56 -0500

    CRUD alumnos

---
+++ bd/alumnosactualizar.php
@@ -0,0 +1,30 @@
+<?php
+include_once('conexion.php');
+$objeto= new Conexion();
+$conexion=$objeto->Conectar();
+
+$ID=$_POST['ID'];
+$NOMBRE=$_POST['NOMBRE'];
+$APELLIDO_PATERNO=$_POST['APELLIDO_PATERNO'];
+$APELLIDO_MATERNO=$_POST['APELLIDO_MATERNO'];
+
+$GRUPO_ID=$_POST['GRUPO_ID'];
+$MATRICULA=$_POST['MATRICULA'];
+
+
+$consulta="UPDATE usuarios SET NOMBRE = '$NOMBRE', APELLIDO_PATERNO = '$APELLIDO_PATERNO', APELLIDO_MATERNO = '$APELLIDO_MATERNO' WHERE ID = '$ID'";
+$resultado=$conexion->prepare($consulta);
+$resultado->execute();
+
+$consulta="UPDATE alumnos SET GRUPO_ID = '$GRUPO_ID', MATRICULA = '$MATRICULA' WHERE USUARIO_ID = '$ID'";
+$resultado=$conexion->prepare($consulta);
+$resultado->execute();
+
+if($resultado->rowCount()>=1){
+	$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+}else{
+	$data="null";
+}
+print json_encode($data);
+$conexion=null;
+?>
