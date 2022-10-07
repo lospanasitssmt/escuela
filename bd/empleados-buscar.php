883a1d8e7b1350c9af398881868d0f8ea44b1145
Anahí <ana.linares0919@gmail.com>
2022-10-06 22:10:33 -0500

    CRUD docentes

---
+++ bd/empleados-buscar.php
@@ -0,0 +1,31 @@
+<?php
+include_once('conexion.php');
+$objeto= new Conexion();
+$conexion=$objeto->Conectar();
+
+$search=$_POST['search'];
+
+$consulta="SELECT 
+                e.ID,  u.NOMBRE, u.APELLIDO_PATERNO, u.APELLIDO_MATERNO, e.CLAVE_EMPLEADO, e.ESPECIALIDAD, e.CEDULA_PROFESIONAL, e.USUARIO_ID, u.TELEFONO, u.EMAIL
+                FROM empleados e
+                INNER JOIN usuarios u ON e.USUARIO_ID=u.ID /*USUARIO_ID */
+                WHERE 
+                e.CLAVE_EMPLEADO LIKE '%$search%' OR
+                e.ESPECIALIDAD LIKE '%$search%' OR 
+
+                u.NOMBRE LIKE '%$search%' OR
+                u.APELLIDO_PATERNO LIKE '%$search%' OR
+                u.APELLIDO_MATERNO LIKE '%$search%'
+                ;";
+
+
+$resultado=$conexion->prepare($consulta);
+$resultado->execute();
+if($resultado->rowCount()>=1){
+	$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+}else{
+	$data="null";
+}
+print json_encode($data);
+$conexion=null;
+?>
---
+++ bd/empleados-insertar.php
@@ -0,0 +1,40 @@
+<?php
+include_once('conexion.php');
+$objeto= new Conexion();
+$conexion=$objeto->Conectar();
+
+// Campos de la tabla usuarios
+$NOMBRE=$_POST['NOMBRE'];
+$APELLIDO_PATERNO=$_POST['APELLIDO_PATERNO'];
+$APELLIDO_MATERNO=$_POST['APELLIDO_MATERNO'];
+$DIRECCION=$_POST['DIRECCION'];
+$TELEFONO=$_POST['TELEFONO'];
+$EMAIL=$_POST['EMAIL'];
+$PASSWORD=$_POST['PASSWORD'];
+
+// Campos de la tabla empleados (docentes)
+$CLAVE_EMPLEADO=$_POST['CLAVE_EMPLEADO'];
+$ULTIMO_GRADO_ESTUDIOS=$_POST['ULTIMO_GRADO_ESTUDIOS'];
+$ESPECIALIDAD=$_POST['ESPECIALIDAD'];
+$CEDULA_PROFESIONAL=$_POST['CEDULA_PROFESIONAL'];
+
+$consulta="INSERT INTO usuarios(NOMBRE, APELLIDO_PATERNO, APELLIDO_MATERNO, DIRECCION, TELEFONO, EMAIL, TIPO_USUARIO_ID, PASSWORD) VALUES('$NOMBRE', '$APELLIDO_PATERNO', '$APELLIDO_MATERNO', '$DIRECCION', '$TELEFONO', '$EMAIL', '3', '$PASSWORD')";
+$resultado=$conexion->prepare($consulta);
+$resultado->execute();
+if($resultado->rowCount()>=1){
+	//$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+
+$IDA=$conexion->lastInsertId();
+$consulta="INSERT INTO empleados(CLAVE_EMPLEADO, ULTIMO_GRADO_ESTUDIOS, ESPECIALIDAD, CEDULA_PROFESIONAL, USUARIO_ID) VALUES('$CLAVE_EMPLEADO', '$ULTIMO_GRADO_ESTUDIOS', '$ESPECIALIDAD', '$CEDULA_PROFESIONAL', '$IDA')";
+$resultado=$conexion->prepare($consulta);
+$resultado->execute();
+    if($resultado->rowCount()>=1){
+        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+    }else{
+        $data="null";    
+    }
+}else{
+	$data="null";
+}
+print json_encode($data);
+$conexion=null;
