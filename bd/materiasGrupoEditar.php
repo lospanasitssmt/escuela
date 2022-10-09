b697408fbcadaab9716ad7a63637218c7eabf1df
Rodrigo <rodcarcherd2128@gmail.com>
2022-10-09 16:51:13 -0500

    Materias

---
+++ bd/materiasGrupoEditar.php
@@ -0,0 +1,21 @@
+<?php
+include_once('conexion.php');
+$objeto= new Conexion();
+$conexion=$objeto->Conectar();
+
+$id=$_POST['id'];
+$idmateria=$_POST['idmateria'];
+$idgrupo=$_POST['idgrupo'];
+
+$consulta="UPDATE MATERIAS_GRUPO SET MATERIAS_DOCENTE_ID = '$idmateria', GRUPOS_ID = '$idgrupo' WHERE ID = '$id'";
+$resultado=$conexion->prepare($consulta);
+$resultado->execute();
+
+if($resultado->rowCount()>=1){
+	$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+}else{
+	$data="null";
+}
+print json_encode($data);
+$conexion=null;
+?>
---
+++ bd/materiasGrupoInsert.php
@@ -0,0 +1,20 @@
+<?php
+include_once('conexion.php');
+$objeto= new Conexion();
+$conexion=$objeto->Conectar();
+
+$idmateria=$_POST['idmateria'];
+$idgrupo=$_POST['idgrupo'];
+
+$consulta="INSERT INTO MATERIAS_GRUPO(MATERIAS_DOCENTE_ID, GRUPOS_ID) VALUES('$idmateria', '$idgrupo')";
+$resultado=$conexion->prepare($consulta);
+$resultado->execute();
+
+if($resultado->rowCount()>=1){
+	$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+}else{
+	$data="null";
+}
+print json_encode($data);
+$conexion=null;
+?>
