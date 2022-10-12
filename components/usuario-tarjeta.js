20c2b4291e3aa88f43c9e8ac887615f7949071ee
Yostin <djycs01@gmail.com>
2022-10-12 00:34:14 -0500

    tarjeta usuarios

--- components/usuario-tarjeta.js
+++ components/usuario-tarjeta.js
@@ -18,43 +18,170 @@ class UsuariosTarjeta extends HTMLElement {
 
         this.divStyle.innerHTML = `
         .contenedor{
-            position: relative;
-            left: 0%;
-            right: 0%;
-            top: 0%;
-            bottom: 0%;
-
-            background-color: red;
-            border-radius: 10px;           
+            background-color: white;
+            border-radius: 10px;
+            padding: 35px 36px;
+            float: left;           
         }
 
-        .categoria{
-            position: absolute;
-            height: 15px;
-            left: 5.49%;
-            right: 6.7%;
-            top: calc(50% - 15px/2 + 0.5px);
+        #total_categoria{
+            padding: 30px;
+        }
 
+        #contenedor_categoria{
+            background-color: #E1E4FF;
+            border-radius: 10px;
+            float: left; 
+            
+        }
+
+        #categoria{
             font-family: 'DM Sans';
             font-style: normal;
             font-weight: 700;
             font-size: 10px;
             line-height: 15px;
-
             text-align: center;
-          
+            color: #000000;
+        }
+
+        #nombre{
+            font-family: 'DM Sans';
+            font-style: normal;
+            font-weight: 500;
+            font-size: 22px;
+            line-height: 29px;
 
             color: #000000;
+        }
+
+        #desc{
+            font-family: 'DM Sans';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 11px;
+            line-height: 20px;
+
+            color: #000000;
+
+            mix-blend-mode: normal;
+            opacity: 0.4;
+        }
+
+        #img{
+            width: 90px;
+            height: 90px;
+
+        }
+        
+        #contenido{
+            display: inline-block;
+        }
+
+        #imagen{
+            display: inline-block;
+        }
+
+        #texto_btn{
+            font-family: 'Manrope';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 12px;
+            line-height: 150%;
+
+            letter-spacing: 0.05em;
+            text-transform: capitalize;
+
+            color: #0D5F00;
+
+            flex: none;
+            order: 1;
+            flex-grow: 0;
+            padding-left: 10px;
+        }
+
+        #img_boton{
+            width: 19px;
+            height: 19px;
+        }
+
+        .boton{
+            display: flex;
+            background: #C5F2C7;
+            border: none;
+            padding: 8.5px 10.6px 8.5px 10.6px;
+            border-radius: 7.82022px;
+            font-family: 'Manrope';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 12px;
+            line-height: 150%;
+            letter-spacing: 0.05em;
+            text-transform: capitalize;
+            color: #0D5F00;
+            cursor: pointer;
+        }
+
+        #btn-complete{
+            padding: 27px 30px 27px 30px;
+        }
+
+        .contenido{
+            padding: 27px 30px 27px 30px;
+        }
+
+        #titulo{
+            padding-left: 30px;
         }
         `;
 
         this.divContent.innerHTML = `
         <div class="contenedor">
-            <div class="categoria">
-                <span>
-                    
-                    ${this.getAttribute("categoria")}
-                </span>
+
+        <div id="total_categoria">
+            <div id="contenedor_categoria">
+                    <span id="categoria">
+                        ${this.getAttribute("categoria")}
+                    </span>
+            </div>
+        </div>    
+            
+            
+        <div id="titulo">
+            <span id="nombre">
+                ${this.getAttribute("nombre")}
+            </span>
+        </div>
+
+
+        <div class=contenido>
+            <div id="contenido">
+                <div>
+                    <span id="desc">
+                        ${this.getAttribute("desc")}
+                    </span>
+                </div>
+            </div> 
+
+
+            <div id="imagen">
+                <div>
+                    <img id="img" src="imagenes/FotoPeril.png">
+                </div>
+            </div>
+        </div>
+
+            <div id="btn-complete">
+                <button class="boton">
+                    <span>
+                        <img id="img_boton" src="imagenes/pen-2.svg">
+                    </span>
+
+                    <span id="texto_btn">
+                        Editar Información
+                    </span>
+                
+                </button>
             </div>
 
             
