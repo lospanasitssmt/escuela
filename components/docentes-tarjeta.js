e4d39d11d467c62b00699e22abcda7fcbd4a6fad
Anahí <ana.linares0919@gmail.com>
2022-10-13 13:45:31 -0500

    docentes

--- components/docentes-tarjeta.js
+++ components/docentes-tarjeta.js
@@ -105,6 +105,18 @@ class DocentesTarjeta extends HTMLElement {
             margin-bottom: 15px;
         }
 
+        .encabezado{
+            font-family: 'Manrope';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 11px;
+            line-height: 15px;
+
+            text-transform: uppercase;
+
+            color: #A3A3A3;
+        }
+
         `;
 
         this.divContent.innerHTML = `
@@ -138,23 +150,185 @@ class DocentesTarjeta extends HTMLElement {
 
             <div id="primer-col">
                 <div id="espacio"> </div>
-                <div id="nombre">
+                <div id="nombre" class="encabezado">
                     <span>NOMBRE</span>
                 </div>
-                <div id="especialidad">
+                <div id="especialidad" class="encabezado">
                     <span>ESPECIALIDAD</span>
                 </div>
-                <div id="grupoT">
+                <div id="grupoT" class="encabezado">
                     <span>GRUPO TUTORADO</span>
                 </div>
-                <div id="acciones"> 
+                <div id="acciones" class="encabezado"> 
                     <span>ACCIONES</span> 
                 </div>
             </div>
+
+            <docentes-tarjetaindividual
+                imagen="imagenes/perfil.jpg"
+                nombre="Alberto Juárez Varela"
+                gradoEstudio="Grado de estudio"
+                especialidad="Arquitecto de Nube"
+                cedula="Cédula Profesional"
+                grupoTutorado="3 A"
+
+            >
+
+            </docentes-tarjetaindividual>
+
+            <docentes-tarjetaindividual
+                imagen="imagenes/perfil.jpg"
+                nombre="Laura González Lara"
+                gradoEstudio="Grado de estudio"
+                especialidad="Inteligencia Artificial"
+                cedula="Cédula Profesional"
+                grupoTutorado="1 B"
+
+            >
+
+            </docentes-tarjetaindividual>
 
         </div>
         `;
     }
 }
 
+//---------------- Inicio de tarjeta individual -----------------------
+class DocentesTarjetaIndividual extends HTMLElement {
+    constructor() {
+        super();
+
+        let shadow = this.attachShadow({
+            mode: 'open'
+        });
+
+        this.divStyle = document.createElement("style");
+        this.divContent = document.createElement("div");
+
+        shadow.appendChild(this.divStyle);
+        shadow.appendChild(this.divContent);
+    }
+
+    connectedCallback() {
+
+
+        this.divStyle.innerHTML = `
+        .contenedor{
+
+        }
+
+        #nombre{
+            display: inline-block;
+            width: 300px;
+        }
+
+        #espacio{
+            display: inline-block;
+            width: 46.5px;
+  
+            text-align: center;
+            transform: translate(0, -60%);
+        }
+
+        #imagen{
+            display: inline-block;
+        }
+
+        #img{
+            width: 57.99px;
+            height: 41.24px;
+            border-radius: 12px;
+        }
+
+        #especialidad{
+            display: inline-block;
+            width: 215px;
+        }
+
+        #texto{
+            display: inline-block;
+        }
+
+        .textoPrincipal{
+            font-family: 'Manrope';
+            font-style: normal;
+            font-weight: 600;
+            font-size: 13px;
+            line-height: 18px;
+
+            color: #000000;
+        }
+
+        .textoSecundario{
+            font-family: 'Manrope';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 12px;
+            line-height: 16px;
+
+            color: #8D8D8D;
+        }
+
+        #grupoT{
+            display: inline-block;
+            width: 205px;
+        }
+
+        #acciones{
+            display: inline-block;
+            width: 115px;
+        }
+
+        `;
+
+        this.divContent.innerHTML = `
+        <div class="contenedor">
+            <hr/>
+
+            <div>
+                <div id="espacio"> 
+                    <input type="checkbox"  >
+                </div>
+
+                <div id="nombre">
+                    <div id="imagen">
+                        <img id="img" src="${this.getAttribute("imagen")}">
+                    </div>
+                    <div id="texto"> 
+                        <div class="textoPrincipal">
+                            ${this.getAttribute("nombre")}
+                        </div>
+                        <div class="textoSecundario">
+                            ${this.getAttribute("gradoEstudio")}
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div id="especialidad">
+                        <div class="textoPrincipal">
+                            ${this.getAttribute("especialidad")}
+                        </div>
+                        <div class="textoSecundario">
+                            ${this.getAttribute("cedula")}
+                        </div>
+                </div>
+
+                <div id="grupoT" class="textoSecundario">
+                    ${this.getAttribute("grupoTutorado")}
+                </div>
+
+                <div id="acciones"> 
+                    
+                </div>
+            </div>
+        </div>
+
+        
+        `;
+    }
+}
+
+
 customElements.define("docentes-tarjeta", DocentesTarjeta);
+customElements.define("docentes-tarjetaindividual", DocentesTarjetaIndividual);
